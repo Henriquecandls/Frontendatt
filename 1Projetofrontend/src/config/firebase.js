fb6826a7e7ba2b764d9b9e884aaa91b3c561ca52
Henriquecandls <henriqueccandelaria@gmail.com>
2026-04-27 17:43:51 +0100

    inicio de configuraçao do firebase

---
+++ 1Projetofrontend/src/config/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDPkUZa6vl7P0JuCrmCPGSBuJb4LFZTKjQ",
+  authDomain: "frontendprojeto1-7eead.firebaseapp.com",
+  projectId: "frontendprojeto1-7eead",
+  storageBucket: "frontendprojeto1-7eead.firebasestorage.app",
+  messagingSenderId: "3326246179",
+  appId: "1:3326246179:web:46cb75c7935dc8acdc6096"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
